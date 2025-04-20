3473ddeceb132b0562fc946835c6b03f71b010ae
Michael Garcia <thecrazygm@gmail.com>
2025-04-20 04:08:49 -0400

    Remove Python version check and update package descriptions

--- engine-bench/src/engine_bench/__init__.py
+++ engine-bench/src/engine_bench/__init__.py
@@ -1,11 +1,4 @@
-"""Nectar-bench package for benchmarking hive-engine nodes using nectarengine."""
-
-import sys
+"""engine-bench package for benchmarking hive-engine nodes using nectarengine."""
 
 __app_name__ = "engine-bench"
 __version__ = "0.2.1"
-
-# Check Python version
-if sys.version_info < (3, 13):
-    print("Error: engine-bench requires Python 3.13 or higher")
-    sys.exit(1)
--- hive-bench/src/hive_bench/__init__.py
+++ hive-bench/src/hive_bench/__init__.py
@@ -1,11 +1,4 @@
-"""Hive node benchmarking package."""
-
-import sys
+"""hive-bench package for benchmarking hive nodes using nectar."""
 
 __app_name__ = "hive-bench"
 __version__ = "0.2.1"
-
-# Check Python version
-if sys.version_info < (3, 13):
-    print("Error: hive-bench requires Python 3.13 or higher")
-    sys.exit(1)
