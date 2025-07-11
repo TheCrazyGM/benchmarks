9e10f9a9cb4e41377bc263dc78811db3ba720843
Michael Garcia <thecrazygm@gmail.com>
2025-07-11 14:54:33 -0400

    maint: remove dead nodes

--- hive-bench/src/hive_bench/utils.py
+++ hive-bench/src/hive_bench/utils.py
@@ -13,15 +13,10 @@ INITIAL_NODES = [
     "https://api.openhive.network",
     "https://techcoderx.com",
     "https://api.c0ff33a.uk",
-    "https://hive-api.3speak.tv",
     "https://hiveapi.actifit.io",
     "https://rpc.mahdiyari.info",
     "https://api.hive.blog",
     "https://anyx.io",
-    "https://hive.roelandp.nl",
-    "https://hived.emre.sh",
-    "https://api.hive.blue",
-    "https://rpc.ausbit.dev",
     "https://hive-api.dlux.io",
 ]
 
